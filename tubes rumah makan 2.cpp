b551723c75eb1581d8272400526cc691c2d5476f
Shafarazhra <95610338+Shafarazhra@users.noreply.github.com>
2021-12-07 08:21:17 +0800

    Update tubes rumah makan 2.cpp

--- tubes rumah makan 2.cpp
+++ tubes rumah makan 2.cpp
@@ -1,41 +1,36 @@
 #include <conio.h>
 #include <stdio.h>
 #include <windows.h>
-#include <string.h>
+#include<stdlib.h>
 
 main()
 {
- float total_harga, kembali, pembayaran, kurang;
- int pilih, banyak_pembelian;
- char menu, nama_pegawai;
- char input_username_pegawai[50];
- char input_password[50];
- char username_pegawai[50];
- char password_pegawai[50];
- 
-printf("Daftar Nama Pegawai\n");
-printf("\t1. Ratna\n");
-printf("\t2. Shafara\n");
-printf("Pilih Nama Pegawai [1/2]:\n");
-scanf("%d, &&nama_pegawai");
+float total_harga, kembali, pembayaran, kurang;
+int pilih, banyak_pembelian;
+char menu;
+char username[8], pass[12];
+int repeat = 0, gagal = 0;
 
-strcpy(username_pegawai, "Ratna");
-strcpy(password_pegawai, "pegawai1");
-strcpy(username_pegawai, "Shafara");
-strcpy(password_pegawai, "pegawai2");
+   while(repeat < 3){
+      printf("Masukkan Username : ");
+      scanf("%s", &username);
+      printf("Masukkan Password : ");
+      scanf("%s", &pass);
 
-	printf("Silahkan Login!\n");
+      if(strcmp(username,"Rasha")==0 && strcmp (pass,"lapar")== 0){
+         printf("Selamat, kamu berhasil login\n");
+         break;
+      }else{
+         printf("Input Salah!\n");
+         gagal++;
+         if(gagal == 3){
+            printf("Sayang sekali, kamu tidak bisa login lagi\n");
+         }
+      }
+      repeat++;
+   }
 
- 	printf("\n username : ");
-	fgets(input_username_pegawai, 50, stdin);
-	printf("\n password :  ");
-	fgets(input_password, 50, stdin);
-	
-	if(strcmp(username_pegawai, "Ratna") == 0 && strcmp(password_pegawai, "pegawai1") == 0){
-	if(strcmp(username_pegawai, "Shafara") == 0 && strcmp(password_pegawai, "pegawai2") == 0) {
-		
-	printf("Login Berhasil!\n\n");
-	
+   system("pause");
 
 
 
